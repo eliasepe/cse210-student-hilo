e6523e867895686d0f51a5e1471fc1c99ea69af5
sharedordaz <sharedordaz@live.com.mx>
2021-10-07 23:47:10 -0500

    commited

--- learning_how_to_class_2.py
+++ learning_how_to_class_2.py
@@ -1,2 +1,15 @@
 """THIS IS A SECOND PART FOR THIS THING. IM SEEING THIS TUTORIAL"""
 """https://www.youtube.com/watch?v=BJ-VvGyQxho"""
+
+
+#Lets use the same Employees class:
+class Employee:
+
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+
+    #Here we are creating a function to create a METHOD to the class. A METHOD is a function that you use in an INSTANCE
+    def fullname(self):
+        return f"First name: {self.first}  Last name: {self.last}"
+
