e10297ec5b5f37888bc0c78770cec8e67d2e996a
sharedordaz <sharedordaz@live.com.mx>
2021-10-08 15:55:37 -0500

    coint_points repared

--- hilo/game/player.py
+++ hilo/game/player.py
@@ -5,7 +5,7 @@ class Player:
     
     present_card = 0
     future_card = 0
-    
+    total_points = [300]
 
     def __init__(self):
         pass
@@ -25,20 +25,21 @@ class Player:
             next_card = "h"
         elif self.present_card >= self.future_card:
             next_card = "l"
-        print (ans, next_card)
+        #print (ans, next_card)
         if ans == next_card:
             result = True
         elif ans != next_card:
             result = False
         return result
 
-    def count_points(self):
-        total_points = [300]
-        if self.guess == True:
-            total_points.append(100)
-        elif self.guess == False:
-            total_points.append(-75)
-        return sum(total_points)
+
+    def count_points(self): 
+        guess = self.guess()      
+        if guess == True:
+            self.total_points.append(100)
+        elif guess == False:
+            self.total_points.append(-75)
+        return sum(self.total_points)
         
 
 shared = Player()
@@ -57,7 +58,9 @@ print(shared.future_card)
 shared.draw()
 
 
-print(shared.guess())
+
+#print(shared.total_points)
 print(shared.count_points())
+#print(shared.total_points)
 
 
