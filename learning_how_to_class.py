3f43855d4b1e14ee41fe2138c5f4669a859fb4d5
sharedordaz <sharedordaz@live.com.mx>
2021-10-07 23:30:22 -0500

    Finished

--- learning_how_to_class.py
+++ learning_how_to_class.py
@@ -13,35 +13,26 @@ print("\nPrinting instances only")
 print(emp_1) #Prints only that this is connected to the "Employee" class. This is emp_1 and emp_2 are INSTANCES
 print(emp_2)
 
-
+#The INSTANCES can have assigned ATTRIBUTES. You can assign ATTRIBUTES with the Instance.attribute. Can have whatever name
 emp_1.first = 'Corey'
 emp_1.last = "Schafer"
 
 emp_2.first = "Juan"
 emp_2.last = "Aldama"
 
-print("\nPrinting emp_1 and emp_2 only")
-print (emp_1) #Stills runs the same, because we have to specify the method we are using in each instance. 
-print (emp_2) 
+
 
 print("\nPrinting emp_1 and emp_2 calling the methods")
-print (emp_1.first, emp_1.last) #Now it prints the first and the last name of each employee, because we are specifing the method
-print (emp_2.first, emp_2.last)
+print (emp_1.first, emp_1.last) #Now it prints the first and the last name of each employee, because we are specifing the atribute
+print (emp_2.first, emp_2.last) #Still we have to specify the ATTRIBUTE, because if we say only the INSTANCE, we will dont get any variable
 
-#A method is like a variable that we can assign to another variable. We call those variables with methods.
-#An instance is just a variable with the value of a class
-
-"""
-one = [1, Employee()]
-print(one)
-one[1].text = "one"
-print(one)
-print(one[1])
-print(one[1].text)"""
-#TODO Ignore this if you want, is just an experiment, of how we can assign variables to "another variable" and call those variables with the methods
+#An ATTRIBUTE is like a variable that we can assign to another variable. We call those variables with methods.
+#An INSTANCE is just a variable with the value of a class
 
 
-#Here, im doing the same, just what im doing is, instead of declaring the methods (.first and .last) in each variable,
+
+
+#Here, im doing the same, just what im doing is, instead of declaring the ATTRIBUTES (.first and .last) in each variable,
 #Im doing that using the "self", so instead of doing this:
 """
 emp_1 = Employee()
@@ -68,7 +59,32 @@ nemp_2 = New_Employee("Juan", "Aldama")
 print("\nInstance only")
 print(nemp_1) #The ouput is telling only that nemp is an INSTANCE of the "New_Employer" class
 
-#But we can see the values again if we print the methods
+#But we can see the values again if we print the ATTRIBUTE
 print("\nInstance with method")
 print(nemp_1.first)
 
+#We can use ATTIBUTES in a format like
+print("Format printing")
+print(f"First name: {nemp_1.first}  Last name: {nemp_1.last}")
+#And it prints out the information
+
+
+#TODO we can also use functions, so lets create a third class to se how this works
+class Final_Employee:
+
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+
+    #Here we are creating a function to create a METHOD to the class. A METHOD is a function that you use in an INSTANCE
+    def fullname(self):
+        return f"First name: {self.first}  Last name: {self.last}"
+
+#We can learn here, that "self" means "whichever INSTANCE you choose to use"
+    
+
+#Lets create an INSTANCE to our class
+femp = Final_Employee("Shared", "Ordaz")
+#So the METHOD is a function that we apply to our INSTANCE, and we call them at the same way as we call the ATTRIBUTES
+print("Printing the METHOD")
+print(femp.fullname())
