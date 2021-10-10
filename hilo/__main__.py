91fbbfce8651159d80d44c7359d06bbdc59269f2
sharedordaz <sharedordaz@live.com.mx>
2021-10-09 21:38:44 -0500

    This program exceds the requirements

--- hilo/__main__.py
+++ hilo/__main__.py
@@ -1,4 +1,9 @@
 from dealer import dealer as dlr
-
+print("Welcome to hilo game\n\n")
+print("          _____                      \n         |A .  | _____               \n         | /.\ ||A ^  | _____        \n         |(_._)|| / \ ||A _  | _____ \n         |  |  || \ / || ( ) ||A_ _ |\n         |____V||  .  ||(_'_)||( v )|\n                |____V||  |  || \ / |\n                       |____V||  .  |\n                              |____V|\n\n") 
+print("You get a card, and you have to guess if the next one will be lower or higher")
+print("You lost 75 points if you lose a turn, but you win 100 points if you win it!")
+print("Good luck!!\n\n")
 gamer = dlr()
 gamer.start_game()
+print("This program exceds the requirements")
